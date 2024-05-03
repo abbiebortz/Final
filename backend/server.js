479d4c5dd2202a8e79c419558f1ee391baf4cf70
Abbie Bortz <abbie@Abbies-MBP.attlocal.net>
2024-05-03 11:00:28 -0400

    dev

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ require('dotenv').config({
   const { getUserByUsername, addUser, updateBudget, getBudgetByUsername } = require('./dbHelpers');
   const { authenticateToken } = require('./auth');
   
-  const PORT = process.env.PORT || 5001;
+  const PORT = process.env.PORT || 8080;
   
   const options = {
     key: fs.readFileSync('key.pem'),
