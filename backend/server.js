026a9ce57b6ca8ec95bd7efe16e5cb35d0bc4304
Abbie Bortz <abbie@Abbies-MBP.attlocal.net>
2024-05-03 09:33:35 -0400

    Changes

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ require('dotenv').config({
   const { getUserByUsername, addUser, updateBudget, getBudgetByUsername } = require('./dbHelpers');
   const { authenticateToken } = require('./auth');
   
-  const PORT = process.env.PORT || 5001;
+  const PORT = process.env.PORT || 8080;
   
   app.post('/api/signup', async (req, res) => {
     const { username, password } = req.body;
