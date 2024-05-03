e4e00e479b03c6815bfdb4decbbe6a0e02f71e0e
Abbie Bortz <abbie@Abbies-MBP.attlocal.net>
2024-05-03 11:36:36 -0400

    ws changes

--- src/websocket.js
+++ src/websocket.js
@@ -1,8 +1,12 @@
-// websocket.js
-const protocol = window.location.protocol.includes('https') ? 'wss' : 'ws';
-const ws = new WebSocket(`${protocol}://${location.host}/ws`);
+// Construct WebSocket URL using environment variables
+const websocketProtocol = window.location.protocol.includes('https') ? 'wss' : 'ws';
+const websocketPort = process.env.WDS_SOCKET_PORT || 443; // Default to 443 if environment variable is not set
+const websocketURL = `${websocketProtocol}://${window.location.hostname}:${websocketPort}/ws`;
 
+// Connect to WebSocket server
+const ws = new WebSocket(websocketURL);
 
+// Event handlers for WebSocket events
 ws.onopen = function(event) {
   console.log('WebSocket connection established');
 };
