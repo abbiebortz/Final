5e54725dd72e0400e462b064ebf1817167d9ab2f
Abbie Bortz <abbie@Abbies-MBP.attlocal.net>
2024-05-03 20:33:40 -0400

    Dev

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -78,7 +78,7 @@ const Dashboard = () => {
         setEditingIndex(-1);
 
         try {
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/update`, {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/api/budget`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
